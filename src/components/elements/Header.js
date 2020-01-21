bccc57e772c8b96c3cd0afbd1c623153cefc2f05
Danielle Alba <danib.sgh@gmail.com>
2020-01-20 19:02:42 -0500

    basic header needs to add links and logo

---
+++ src/components/elements/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from '@reach/router';
+import { StyledHeader } from '../styles/StyledHeader'
+
+
+const Header = () => (
+
+    <StyledHeader>
+    <div>
+        TEST THE HEADER TEEEEESSSSSTTTT
+        
+    </div>
+    </StyledHeader>
+
+)
+
+export default Header;
---
+++ src/components/styles/StyledHeader.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const StyledHeader = styled.div`
+font-family: 'Abel', sans-serif;
+  color: #fff;
+  background: #1c1c1c;
+  padding: 0 20px;
+  box-sizing: border-box;
+
+  .header-content {
+    max-width: 1280px;
+    min-height: 120px;
+    padding: 20px 0px;
+    margin: 0 auto;
+    box-sizing: border-box;
+
+    @media screen and (max-width: 500px) {
+      max-width: 1280px;
+      min-height: 0px;
+    }
+  }
+`;
+
+
